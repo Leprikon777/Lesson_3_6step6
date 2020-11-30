c88b5465f8113e2f1e06d603156264a9f19ba6fa
a.grizenko <a.grizenko@digital-sector.ru>
2020-11-30 11:09:07 +0300

    Добавил assert

--- test_items.py
+++ test_items.py
@@ -1,8 +1,17 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+
+def is_element_present(browser):
+    try:
+        browser.implicitly_wait(10)
+        browser.find_element_by_css_selector(".btn-add-to-baske")
+        return True
+    except:
+        return False
+
 def test_guest_should_see_basket_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
     time.sleep(10)
+    assert is_element_present(browser) == True, "Не найдена кнопка 'Добавить в корзину'"
 
